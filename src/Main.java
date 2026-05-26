86f4ae9b540fc66c69712cdf4694c9c11719f568
bharat2005 <bharatvdeshm2005@gmail.com>
2026-05-26 16:59:29 +0530

    gray code 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i <= (int) Math.pow(2.0, n); i++) {
+        for (int i = 0; i < (int) Math.pow(2.0, n); i++) {
             int gray = i ^ (i >> 1);
 
-            String s = Integer.toString(gray);
-
-            while (s.length() < n) {
-                s = "0" + s;
+            String binary = Integer.toBinaryString(gray);
+            while (binary.length() < n) {
+                binary = "0" + binary;
             }
 
-            System.out.println(s);
+            System.out.println(binary);
         }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,8 @@ import kotlin.math.pow
 fun main(){
     val n = readLine()!!.toInt()
 
-    for(i in 0..2.0.pow(n).toInt()) {
+    for(i in 0 until 2.0.pow(n).toInt()) {
         val gray = i xor (i shr 1)
-        println(gray.toString().padStart(n, '0'))
+        println(gray.toString(2).padStart(n, '0'))
     }
 }
