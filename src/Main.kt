94b269459f315adde6b51b25e5b929485fed22fe
bharat2005 <bharatvdeshm2005@gmail.com>
2026-05-26 17:21:31 +0530

    gray code 3

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,16 @@ fun main(){
 
     for(i in 0 until 2.0.pow(n).toInt()) {
         val gray = i xor (i shr 1)
-        println(gray.toString(2).padStart(n, '0'))
+        val str = StringBuilder()
+
+        for(j in 0 until n){
+            if((gray shr j) and 1 == 1){
+                str.append("1")
+            } else {
+                str.append("0")
+            }
+        }
+
+        println(str.toString())
     }
 }
