8ebe8709c1b61e167c616184d75ca071cab2d703
bharat2005 <bharatvdeshm2005@gmail.com>
2026-05-27 13:43:23 +0530

    numberOfSpecialChars 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,28 @@
 
 fun main(){
-    val n = readLine()!!.toInt()
-    val str = readLine()!!
 
-    var currLen = 1
-    var maxLen = 0
-    for(i in 1 until n){
-        if(str[i] == 'x' && str[i-1] == 'x' ){
-            currLen++
-        } else{
-            currLen = 1
+    fun numberOfSpecialChars(word: String): Int {
+
+        val lowerCase = IntArray(26) { Int.MIN_VALUE}
+        val upperCase = IntArray(26) { Int.MAX_VALUE }
+
+        for(i in word.indices){
+            val ch = word[i]
+            if(ch in 'a'..'z'){
+                lowerCase[ch - 'a'] = i
+            } else {
+                upperCase[ch - 'A'] = minOf(upperCase[ch - 'A'], i)
+            }
         }
 
-        maxLen = maxOf(currLen, maxLen)
-    }
+        var count = 0
+        for(i in 0 until 26){
+            if(lowerCase[i] != Int.MIN_VALUE && upperCase[i] != Int.MAX_VALUE
+                && lowerCase[i] < upperCase[i]){
+                count++
+            }
+        }
 
-    val res = if(maxLen == 1) 0 else maxLen - 2
-    println(res)
+        return count
+    }
 }
