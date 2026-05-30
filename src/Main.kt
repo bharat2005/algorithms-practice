cfb998ed2f6544db48ace582fda8569e96215247
bharat2005 <bharatvdeshm2005@gmail.com>
2026-05-30 20:35:50 +0530

    cf - alice-bob choc 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,6 @@
-import kotlin.math.abs
 
 fun main(){
-    val n = readLine()!!.toInt()
-    val arr = readLine()!!.split(" ").map { it.toInt() }.sorted()
-//    val n = 5
-//    val arr = intArrayOf(3, 2, 7, 4, 1).sortedDescending()
 
-    var g1 = arr[0]
-    var g2 = arr[1]
-
-    for( i in 2 until n){
-        val num = arr[i]
-        if(abs(g1 + num - g2 ) < abs(g2 + num - g1)){
-         //   println("${abs(g1 + num - g2 )} < ${abs(g2 + num - g1)}")
-            g1 += num
-        } else {
-         //   println("${abs(g1 + num - g2 )} > ${abs(g2 + num - g1)}")
-           g2 += num
-        }
-    }
-
-    println(abs(g1 - g2))
 }
+
+
--- src/main/kotlin/problems/c_alice_bob_and_chocolate/KSolution.kt
+++ src/main/kotlin/problems/c_alice_bob_and_chocolate/KSolution.kt
@@ -1,26 +1,28 @@
+package problems.c_alice_bob_and_chocolate
+
 import java.util.Scanner
 
 fun main() {
     val n = readLine()!!.toInt()
     val arr = readLine()!!.split(" ").map { it.toLong() }
-    
+
     //create pfx sum arr
     val pfxSum = LongArray(n)
     pfxSum[0] = arr[0]
     for(i in 1 until n){
         pfxSum[i] = arr[i] + pfxSum[i - 1]
     }
-    
+
     //targeted middle value
     val target = pfxSum[n-1] / 2
-    
+
     //binary search
     var right = n-1
     var left = 0
     var ans = Int.MAX_VALUE
     while(left <= right){
         var mid = left + (right - left) / 2
-    
+
         if(target <= pfxSum[mid]){
             ans = mid
             right = mid - 1
@@ -28,7 +30,7 @@ fun main() {
             left = mid + 1
         }
     }
-    
+
     println("${ans} ${n - ans}")
-    
+
 }
