c8964026ceef6f738860a0f8da57ecbd59f91e3b
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-01 23:31:29 +0530

    dl

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,4 @@
 fun main() {
 
-    fun minimumCost(cost: IntArray): Int {
-        val ncost = cost.sorted()
-        val n = ncost.size
-
-        var money = 0
-        var left = 0
-        var right = n - 1
-
-
-        while(left < right && left in 0..n-1 && right in 0..n-1){
-            money += ncost[left] + ncost[right]
-            right -= 2
-            left--
-        }
-
-
-        if(ncost.size % 2 != 0 && left in 0..n-1){
-            money += ncost[left]
-        }
-
-        return money
-    }
-
 
 }
