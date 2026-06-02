61a1abe001cf15743d816fd1a481c0273fade26b
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-02 21:59:57 +0530

    tah 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,32 +1,10 @@
-import java.lang.StringBuilder
-
 fun main() {
-    val t = readLine()!!.toInt()
-    val out = StringBuilder()
+    val n = readLine()!!.toInt()
+    val k = readLine()!!.toInt()
+    val x = readLine()!!.toInt()
+    val y = readLine()!!.toInt()
 
-    repeat(t) {
-        val (n, k) = readLine()!!.split(" ").map { it.toLong() }
+    val total = minOf(n, k) * x + maxOf(0, n - k) * y
 
-        if (n >= k && n % 2 == k % 2) {
-            out.append("YES\n")
-
-            repeat((k - 1).toInt()) {
-                out.append("1 ")
-            }
-            out.append(n - (k - 1)).append('\n')
-
-        } else if (n % 2 == 0L && n >= 2 * k) {
-            out.append("YES\n")
-
-            repeat((k - 1).toInt()) {
-                out.append("2 ")
-            }
-            out.append(n - 2 * (k - 1)).append('\n')
-
-        } else {
-            out.append("NO\n")
-        }
-    }
-
-    print(out.toString())
+    println(total)
 }
