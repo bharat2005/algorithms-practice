530e332db04a4dd54db71ddf8ba54d7c47f3b9e9
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-03 22:07:45 +0530

    roll dice 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,17 @@
 fun main() {
-    fun earliestFinishTime(landStartTime: IntArray, landDuration: IntArray, waterStartTime: IntArray, waterDuration: IntArray): Int {
-        val n = landStartTime.size
-        val m = waterStartTime.size
+    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
 
-        var ans1 = Int.MAX_VALUE
-        for(i in 0 until n){
-            ans1 = minOf(
-                ans1,
-                landStartTime[i] + landDuration[i]
-            )
-        }
+    val fav = 6 - maxOf(x, y) + 1
 
-        var ans2 = Int.MAX_VALUE
-        for(i in 0 until m){
-            ans2 = minOf(
-                maxOf(ans1, waterStartTime[i]) + waterDuration[i],
-                ans2
-            )
-        }
+    var a = fav
+    var b = 6
 
-
-        var ans3 = Int.MAX_VALUE
-        for(i in 0 until m){
-            ans3 = minOf(
-                ans3,
-                waterStartTime[i] + waterDuration[i]
-            )
-        }
-
-        var ans4 = Int.MAX_VALUE
-        for(i in 0 until n){
-            ans4 = minOf(
-                maxOf(ans3, landStartTime[i]) + landDuration[i],
-                ans4
-            )
-        }
-        return minOf(ans2, ans4)
+    while(b != 0){
+        val temp = a
+        a = b
+        b = temp % b
     }
+
+    println("${fav/a}/${6/a}")
+
 }
