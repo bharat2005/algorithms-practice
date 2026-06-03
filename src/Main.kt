5007ee4876638f81f3317926fd523645e61c0bbe
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-03 21:37:40 +0530

    rides 2 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,40 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val k = readLine()!!.toInt()
-    val x = readLine()!!.toInt()
-    val y = readLine()!!.toInt()
+    fun earliestFinishTime(landStartTime: IntArray, landDuration: IntArray, waterStartTime: IntArray, waterDuration: IntArray): Int {
+        val n = landStartTime.size
+        val m = waterStartTime.size
 
-    val total = minOf(n, k) * x + maxOf(0, n - k) * y
+        var ans1 = Int.MAX_VALUE
+        for(i in 0 until n){
+            ans1 = minOf(
+                ans1,
+                landStartTime[i] + landDuration[i]
+            )
+        }
 
-    println(total)
+        var ans2 = Int.MAX_VALUE
+        for(i in 0 until m){
+            ans2 = minOf(
+                maxOf(ans1, waterStartTime[i]) + waterDuration[i],
+                ans2
+            )
+        }
+
+
+        var ans3 = Int.MAX_VALUE
+        for(i in 0 until m){
+            ans3 = minOf(
+                ans3,
+                waterStartTime[i] + waterDuration[i]
+            )
+        }
+
+        var ans4 = Int.MAX_VALUE
+        for(i in 0 until n){
+            ans4 = minOf(
+                maxOf(ans3, landStartTime[i]) + landDuration[i],
+                ans4
+            )
+        }
+        return minOf(ans2, ans4)
+    }
 }
