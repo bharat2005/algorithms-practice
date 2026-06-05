f91451ac513a751695f44df8e47f3cee7881fa00
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-05 13:33:16 +0530

    adj swap 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,4 @@
 fun main() {
-    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-    val arr = readLine()!!.split(" ").map { it.toInt() }
-    val map = mutableMapOf<Int, Int>()
 
-
-    for(i in arr.indices){
-        val std = arr[i]
-        map[std] = map.getOrDefault(std, 0) + 1
-    }
-
-    var ans = 0
-    for(i in map.keys){
-       ans++
-    }
-
-    if(ans == k){
-        println("YES")
-        map.keys.forEach { print(it) }
-    }else {
-        println("NO")
-    }
 
 }
--- src/main/kotlin/problems/a_adjacent_replacements/KSolution.kt
+++ src/main/kotlin/problems/a_adjacent_replacements/KSolution.kt
@@ -1,23 +1,23 @@
+package problems.a_adjacent_replacements
+
 import java.util.Scanner
 
 fun main() {
     val n = readLine()!!.toInt()
     val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
-    
+
     for(i in 1..arr[n-1] step 2) {
         for(j in 0 until n){
             if(arr[j] == i){
                 arr[j] = i + 1
             }
-        }
-    
-        for(j in 0 until n){
             if(arr[j] == i + 1){
                 arr[j] = i
             }
         }
+
     }
-    
-    
+
+
     arr.forEach { print("$it ") }
 }
