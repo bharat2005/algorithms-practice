8405555d868098f0d4a2d6bc2ceaf5b4961f5abe
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-16 23:28:43 +0530

    life without zeros - arthmetic approach

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,29 @@
 
 fun main() {
+
+
+    fun removeZeros(num : Int) : Int {
+        var x = num
+        var result = 0
+        var dy10 = 1
+
+        while(x > 0 ){
+            val digit = x % 10
+            if(digit != 0){
+                result += digit * dy10
+                dy10 *= 10
+            }
+            x /= 10
+        }
+        return result
+    }
+
     val op1 = readLine()!!.toInt()
     val op2 = readLine()!!.toInt()
     val sum = op1 + op2
 
-    //without zeros
-    val op1x = op1.toString().filter { it != '0' }.toInt()
-    val op2x = op2.toString().filter { it != '0' }.toInt()
-    val sumx = sum.toString().filter { it != '0' }.toInt()
 
-
-    if(op1x + op2x == sumx) println("YES") else println("NO")
+    if(removeZeros(op1) + removeZeros(op2) == removeZeros(sum)) println("YES") else println("NO")
 }
 
 
