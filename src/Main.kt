15a5c86031e2058455a73675212d11fc52593348
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-23 12:59:37 +0530

    facetook wall 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,40 @@
-
 fun main() {
+    val me = readLine()!!
+    val n = readLine()!!.toInt()
+    val map = mutableMapOf<String, Int>()
+    val set = mutableSetOf<String>()
 
-
-    fun removeZeros(num : Int) : Int {
-        var x = num
-        var result = 0
-        var dy10 = 1
-
-        while(x > 0 ){
-            val digit = x % 10
-            if(digit != 0){
-                result += digit * dy10
-                dy10 *= 10
-            }
-            x /= 10
+    repeat(n){
+        val arr = readLine()!!.split(" ")
+        val x = arr[0]
+        val action = arr[1]
+        val y = if(arr[1] == "likes"){
+            arr[2].removeSuffix("'s")
+        } else
+        {
+            arr[3].removeSuffix("'s")
         }
-        return result
+        val priority = when(action){
+            "likes" -> 5
+            "commented" -> 10
+            "posted" -> 15
+            else -> 0
+        }
+
+
+        if(me == x) {
+            map[y] = map.getOrDefault(y, 0) + priority
+        } else if(me == y) {
+            map[x] = map.getOrDefault(x, 0) + priority
+        } else {
+            set.add(x)
+            set.add(y)
+        }
     }
 
-    val op1 = readLine()!!.toInt()
-    val op2 = readLine()!!.toInt()
-    val sum = op1 + op2
 
-
-    if(removeZeros(op1) + removeZeros(op2) == removeZeros(sum)) println("YES") else println("NO")
+    map.entries.sortedBy { it.value}.forEach { println(it.key) }
+    set.sorted().forEach { println(it) }
 }
 
 
