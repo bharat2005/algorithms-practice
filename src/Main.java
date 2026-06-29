52adc2117c16f5a27a29f3b6e380f273ffae35c4
bharat2005 <bharatvdeshm2005@gmail.com>
2026-06-29 15:44:24 +0530

    8 q extra 1

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
+    static char[][] arr;
     static int n;
-    static long[] ls;
-    static long total;
-    static long min = Long.MAX_VALUE;
+    static boolean[] column;
+    static boolean[] dig1;
+    static boolean[] dig2;
+    static int ans = 0;
 
-    static void dfs(int currIdx, long currSum) {
-        if (currIdx == n) {
-            // update final anser min
-            min = Math.min(min, Math.abs(total - currSum - currSum));
+    static void dfs(int rowIndex) {
+        // Base case
+        if (rowIndex == n) {
+            ans++;
             return;
         }
 
-        dfs(currIdx + 1, currSum + ls[currIdx]);
-        dfs(currIdx + 1, currSum);
+        // Recursive part
+        for (int i = rowIndex; i < n; i++) {
+
+            // conditions --> row + column + dig1 + dig2 + extra condition
+            boolean isExtraRestricted = arr[rowIndex][i] == '.';
+            boolean isColumnRestricted = column[i];
+            boolean isDig1Restricted = dig1[rowIndex + i];
+            boolean isDig2Restricted = dig2[rowIndex - i + 7];
+
+            if (isColumnRestricted || isDig1Restricted || isDig2Restricted || isExtraRestricted) {
+                continue;
+            }
+
+            // mark restricted
+            column[i] = true;
+            dig1[rowIndex + i] = true;
+            dig2[rowIndex - i + 7] = true;
+
+            dfs(rowIndex + 1);
+
+            // unmark
+            column[i] = false;
+            dig1[rowIndex + i] = false;
+            dig2[rowIndex - i + 7] = false;
+        }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-
-        String[] parts = br.readLine().split(" ");
-        ls = new long[n];
-
-        for (int i = 0; i < n; i++) {
-            ls[i] = Long.parseLong(parts[i]);
-            total += ls[i];
+        arr = new char[8][];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = br.readLine().toCharArray();
         }
 
-        dfs(0, 0);
+        n = arr.length;
+        column = new boolean[n];
+        dig1 = new boolean[n * 2 - 1];
+        dig2 = new boolean[n * 2 - 1];
 
-        System.out.println(min);
+        dfs(0);
+
+        System.out.print(ans);
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,53 @@
 import kotlin.math.abs
 
 fun main() {
-    val n = readLine()!!.toInt()
-    val ls = readLine()!!.split(" ").map { it.toLong() }
-    var total = ls.sum()
-    var min = Long.MAX_VALUE // exceeds int range
+    val arr = Array(8) {
+        readLine()!!.toCharArray()
+    }
+    val n = arr.size
+    val column = BooleanArray(n)
+    var ans = 0
+    val dig1 = BooleanArray(n * 2 - 1)
+    val dig2 = BooleanArray(n * 2 - 1)
 
-    fun dfs(currIdx : Int, currSum : Long) : Unit {
-        if(currIdx == n){
-            //update final anser min
-            min = minOf(min, abs(total - currSum - currSum))
+    fun dfs(rowIndex : Int) : Unit {
+        //Base case
+        if(rowIndex == n){
+            ans++
             return
         }
 
-        dfs(currIdx + 1,currSum + ls[currIdx] )
-        dfs(currIdx + 1, currSum )
+        //Recursive part
+        for(i in rowIndex until n){
+
+            //conditions --> row + column + dig1 + dig2 + extra condition
+            val isExtraRestricted = arr[rowIndex][i] == '.'
+            val isColumnRestricted = column[i]
+            val isDig1Restricted = dig1[rowIndex + i]
+            val isDig2Restricted = dig2[rowIndex - i + 7]
+
+            if(isColumnRestricted || isDig1Restricted || isDig2Restricted || isExtraRestricted) continue
+
+
+            //mark restricted
+            column[i] = true
+            dig1[rowIndex + i] = true
+            dig2[rowIndex - i + 7] = true
+
+
+            dfs(rowIndex + 1)
+
+
+            //unmark
+            column[i] = false
+            dig1[rowIndex + i] = false
+            dig2[rowIndex - i + 7] = false
+
+        }
     }
 
-    dfs(0, 0)
+    print(ans)
 
-    println(min)
 }
 
 
