edca4ef9df45074a2ff562359e50bb32efd50a58
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-02 15:54:21 +0530

    least sum path 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,55 +1,57 @@
-import kotlin.math.abs
+val dr = intArrayOf(0,1,0,-1)
+val dc = intArrayOf(1,0,-1,0)
+
 
 fun main() {
-    val arr = Array(8) {
-        readLine()!!.toCharArray()
-    }
-    val n = arr.size
-    val column = BooleanArray(n)
-    var ans = 0
-    val dig1 = BooleanArray(n * 2 - 1)
-    val dig2 = BooleanArray(n * 2 - 1)
+    fun findSafeWalk(grid: List<List<Int>>, health: Int): Boolean {
+        val m = grid.size
+        val n = grid[0].size
+        val best = Array(m) { IntArray(n) { Int.MAX_VALUE } }
+        val dq = ArrayDeque<IntArray>()
 
-    fun dfs(rowIndex : Int) : Unit {
-        //Base case
-        if(rowIndex == n){
-            ans++
-            return
+        best[0][0] = grid[0][0]
+        dq.addFirst(intArrayOf(grid[0][0], 0, 0))
+
+        while (dq.isNotEmpty()) {
+            val (sum, r, c) = dq.removeFirst()
+
+            //check ends
+            if(r == m-1 && c == n-1){
+                return if ( health - sum > 1) true else false
+            }
+
+            //check adj
+            for (i in 0 until 4) {
+                val nr = r + dr[i]
+                val nc = c + dc[i]
+
+                //check bounds
+                if(nr !in 0..m-1 && nc !in 0..n-1)  continue
+
+                //update best
+                val nsum = best[r][c] + grid[nr][nc]
+                if(nsum < best[nr][nc]) {
+                    best[nr][nc] = nsum
+                    //manage dq
+                    if (grid[nr][nc] == 0) {
+                        dq.addFirst(intArrayOf(nsum, nr, nc))
+                    } else {
+                        dq.addLast(intArrayOf(nsum, nr, nc))
+                    }
+                }
+
+
+            }
         }
 
-        //Recursive part
-        for(i in 0 until n){
 
-            //conditions --> row + column + dig1 + dig2 + extra condition
-            val isExtraRestricted = arr[rowIndex][i] == '*'
-            val isColumnRestricted = column[i]
-            val isDig1Restricted = dig1[rowIndex + i]
-            val isDig2Restricted = dig2[rowIndex - i + 7]
+        //edge case of m=1;n=1
+        return if(health - best[m-1][n-1] > 1) true else false
 
-            if(isColumnRestricted || isDig1Restricted || isDig2Restricted || isExtraRestricted) continue
-
-
-            //mark restricted
-            column[i] = true
-            dig1[rowIndex + i] = true
-            dig2[rowIndex - i + 7] = true
-
-
-            dfs(rowIndex + 1)
-
-
-            //unmark
-            column[i] = false
-            dig1[rowIndex + i] = false
-            dig2[rowIndex - i + 7] = false
-
-        }
     }
-    dfs(0)
-
-    print(ans)
 
 }
+
 
 
 
