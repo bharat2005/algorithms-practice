f38db36deb9f316ce62c28061e6a80c2903b60d8
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-05 17:02:19 +0530

    max score & paths 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,52 +1,49 @@
-import java.util.PriorityQueue
-
-
-
 
 fun main() {
-    fun findSafeWalk(grid: List<List<Int>>, health: Int): Boolean {
-        val dr = intArrayOf(0,1,0,-1)
-        val dc = intArrayOf(1,0,-1,0)
-        val m = grid.size
-        val n = grid[0].size
-        val best = Array(m) { IntArray(n) { Int.MAX_VALUE } }
-        val pq = PriorityQueue<IntArray>()
 
-        best[0][0] = grid[0][0]
-        pq.offer(intArrayOf(grid[0][0], 0, 0))
+    val dr = intArrayOf(0, 1, 1)
+    val dc = intArrayOf(1, 0, 1)
 
-        while (pq.isNotEmpty()) {
-            val (sum, r, c) = pq.poll()
+    fun pathsWithMaxScore(board: List<String>): IntArray {
+        val n = board.size
+        val m = board[0].length
+        val scoreboard = Array(n) { IntArray(m){ -1 } }
+        val pathsboard = Array(n) { IntArray(n) }
+        scoreboard[n-1][m-1] = 0
+        pathsboard[n-1][m-1] = 1
 
-            //check ends
-            if(r == m-1 && c == n-1){
-                return if ( health - sum > 1) true else false
-            }
+        for( r in n-1 downTo 0){
+            for(c in m-1 downTo 0) {
+                if (board[r][c] == 'X' || board[r][c] == 'S') continue
 
-            //check adj
-            for (i in 0 until 4) {
-                val nr = r + dr[i]
-                val nc = c + dc[i]
+                var bestScore = -1
+                var bestPath = 0
 
-                //check bounds
-                if(nr !in 0..m-1 && nc !in 0..n-1)  continue
+                for (i in 0..2) {
+                    val nr = r + dr[i]
+                    val nc = c + dc[i]
+                    if (nr >= n || nc >= m) continue
 
-                //update best
-                val nsum = best[r][c] + grid[nr][nc]
-                if(nsum < best[nr][nc]) {
-                    best[nr][nc] = nsum
+                    val thisScore = board[nr][nc] - '0'
+                    if (thisScore == -1) continue
 
-                    //manage dq
-                    pq.offer(intArrayOf(nsum, nr, nc))
+                    if (thisScore > bestScore) {
+                        bestScore = thisScore
+                        bestPath = pathsboard[nr][nc]
+                    } else if (thisScore == bestScore) {
+                        bestPath += pathsboard[nr][nc]
+                    }
                 }
 
+                scoreboard[r][c] = board[r][c] - '0' + bestScore
+                pathsboard[r][c] = bestPath
 
             }
         }
 
 
-        //edge case of m=1;n=1
-        return if(health - best[m-1][n-1] > 0) true else false
+
+        return if(pathsboard[0][0] == 0) intArrayOf(0,0) else intArrayOf(scoreboard[0][0],pathsboard[0][0])
 
     }
 
