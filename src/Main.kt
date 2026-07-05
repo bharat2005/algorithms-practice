1962baddf7721a7812aa81660312fd8aec0a835c
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-05 17:16:11 +0530

    max score & paths 4

--- src/Main.kt
+++ src/Main.kt
@@ -5,10 +5,11 @@ fun main() {
     val dc = intArrayOf(1, 0, 1)
 
     fun pathsWithMaxScore(board: List<String>): IntArray {
+        val MOD = 1_000_000_000 + 7
         val n = board.size
         val m = board[0].length
         val scoreboard = Array(n) { IntArray(m){ -1 } }
-        val pathsboard = Array(n) { IntArray(n) }
+        val pathsboard = Array(n) { IntArray(m) }
         scoreboard[n-1][m-1] = 0
         pathsboard[n-1][m-1] = 1
 
@@ -31,7 +32,7 @@ fun main() {
                         bestScore = thisScore
                         bestPath = pathsboard[nr][nc]
                     } else if (thisScore == bestScore) {
-                        bestPath += pathsboard[nr][nc]
+                        bestPath = (bestPath + pathsboard[nr][nc]) % MOD
                     }
                 }
 
