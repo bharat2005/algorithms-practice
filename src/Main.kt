742f1458bc145aa80cf7598df9a967b5de9fd5c2
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-07 23:01:31 +0530

    .

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,6 @@
 
 fun main() {
-    val t = readLine()!!.toInt()
 
-    repeat(t){
-        val n = readLine()!!.toInt()
-        val arr = readLine()!!.split(" ").map { it.toInt() }
-
-        var minTwoCount = 0
-        var flag = false
-
-        for(x in arr){
-            if(x >= 2) minTwoCount++
-
-            if(minTwoCount >= 2 || x >= 3){
-                flag = true
-                break
-            }
-        }
-        if(flag) {
-            println("YES")
-        } else {
-            println("NO")
-        }
-    }
 }
 
 
