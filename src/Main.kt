78984394cfd0b6b7f8c6c9e97b74e5a48ffaa640
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-22 23:03:33 +0530

    a and p c

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,12 @@
+import java.util.StringTokenizer
 
 fun main() {
+    val st = StringTokenizer(readln())
+    val set = HashSet<Int>()
 
+    repeat(3) {
+        set.add(st.nextToken().toInt())
+    }
+
+    println(set.size)
 }
-
-
-
-
-
-
