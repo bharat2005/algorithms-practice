c952c216288b4b092aea9e40b5a8645891e63490
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-24 18:24:00 +0530

    r to i 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,32 @@
 
-import java.util.StringTokenizer
-
 fun main() {
-    val n = readln().toInt()
-    val st = StringTokenizer(readln())
+    fun romanToInt(s: String): Int {
+        val map = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+        var sum = map[s[0]]!!
 
-    val freq = IntArray(101)
-    var ans = 0
+        for(i in s.indices){
+            val prevCh = s[i-1]
+            val currCh = s[i]
+            val preVal = map[prevCh]!!
+            val currVal = map[currCh]!!
 
-    repeat(n) {
-        val x = st.nextToken().toInt()
-        freq[x]++
-        ans = maxOf(ans, freq[x])
+            if(preVal >= currVal){
+                sum += currVal
+            } else {
+                sum -= preVal
+                sum += currVal - preVal
+            }
+
+        }
+
+        return sum
     }
-
-    println(ans)
 }
