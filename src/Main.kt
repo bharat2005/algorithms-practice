fa79136db3a7d4f22c2d98cbb8ecb97277eb8bf6
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-26 16:59:35 +0530

    cards 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,35 @@
 
 fun main() {
-    val (m, d) = readLine()!!.split(" ").map { it.toInt() }
-    val s = readLine()!!
-    var c = 0
-    var i = 0
+    val t = readLine()!!.toInt()
 
-    //keep iterate over s
-    while (i < m){
-
-        if(s[i] != 'G'){
-            //keep counting '.'
-            c++
-        } else {
-            //if caught 'G' then remove m count from c
-            c-= d
-
-            //try to skip the remaining part
-            var x = i
-            while(i < m && i <= x + d){
-
-                //if encountred 'G' while skipping then update x and continue skipping fromwards
-                if(s[i] == 'G') {
-                    x = i
-                } else {
-                    i++
-                }
-
-            }
+    for(c in 1..t){
+        val (n, a, b) = readLine()!!.split(" ").map { it.toInt() }
+        //fesibility check
+        if(a + b > n || a >= n || b >= n) {
+            println("NO")
+            continue
         }
-        i++
+
+        //defulat a wins permutation
+        for(i in 1..n){
+            print("${i} ")
+        }
+        println()
+
+        //dynamic b wins permutation
+        //ties if needed
+        val t = n - (a + b)
+        for(i in 1..t){
+            print("${i} ")
+        }
+
+        //rest targeted shift
+        val s = n - a
+        for(i in t+1..n){
+            var u = (i - s + n) % n
+            if(u == 0) u = n
+            print("${u} ")
+        }
 
     }
-
-    println(c)
-
 }
