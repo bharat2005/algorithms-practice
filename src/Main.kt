8b1e87175c604bbfb0de927d640dbd3ff50fbd2f
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-27 23:53:45 +0530

    mx pr 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,76 +1,57 @@
-//
-//fun main() {
-//    val t = readLine()!!.toInt()
-//
-//    for(c in 1..t){
-//        val (n, a, b) = readLine()!!.split(" ").map { it.toInt() }
-//        //fesibility check
-//        if(a + b > n || a >= n || b >= n) {
-//            println("NO")
-//            continue
-//        } else{
-//            println("YES")
-//        }
-//
-//        //defulat a wins permutation
-//        for(i in 1..n){
-//            print("${i} ")
-//        }
-//        println()
-//
-//        //dynamic b wins permutation
-//        //ties if needed
-//        val t = n - (a + b)
-//        for(i in 1..t){
-//            print("${i} ")
-//        }
-//
-//        //rest targeted shift
-//        for(i in t+1..n){
-//            var u = i - b
-//            if(u <= t) u = i + 1
-//            print("${u} ")
-//        }
-//
-//    }
-//}
 
-
-fun main(){
+fun main() {
     val t = readLine()!!.toInt()
 
-    repeat(t){
-        val n = readLine()!!.toInt()
-        if(n % 2 == 1) {
+    for(c in 1..t){
+        val (n, a, b) = readLine()!!.split(" ").map { it.toInt() }
+        //fesibility check
+        if(a + b > n || a >= n || b >= n) {
             println("NO")
-            return@repeat
-        }
-        val ls = readLine()!!.split(" ").map { it.toInt() }
-        var bigs = Int.MAX_VALUE
-        var smalls = Int.MIN_VALUE
-
-        for(i in 1 until n step 2){
-            val big = ls[i-1]
-            val small = ls[i]
-            if(small >= big) {
-                println("NO")
-                return@repeat
-            }
-
-            bigs = minOf(bigs, big)
-            smalls = maxOf(smalls, small)
-
-            if(bigs - smalls > 1 && bigs > smalls){
-                //valid
-            } else {
-                println("NO")
-                return@repeat
-            }
-
+            continue
+        } else{
+            println("YES")
         }
 
-        println("YES")
+        //defulat a wins permutation
+        for(i in 1..n){
+            print("${i} ")
+        }
+        println()
+
+        //dynamic b wins permutation
+        //ties if needed
+        val t = n - (a + b)
+        for(i in 1..t){
+            print("${i} ")
+        }
+
+        //rest targeted shift
+        for(i in t+1..n){
+            var u = i - b
+            if(u <= t) u = i + 1
+            print("${u} ")
+        }
+
     }
 }
+
+
+    fun maxProduct(nums: IntArray): Int {
+        var max1 = 0
+        var max2 = 0
+
+        for (num in nums) {
+            if (num >= max1) {
+                max2 = max1
+                max1 = num
+            } else if (num > max2) {
+                max2 = num
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1)
+    }
+
+
 
 
