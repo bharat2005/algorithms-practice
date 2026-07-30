daf8a22f0d215d921c8b8e99fe05fd2f19e9e948
bharat2005 <bharatvdeshm2005@gmail.com>
2026-07-30 23:57:15 +0530

    min n 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,15 @@
 
 fun main(){
-    val n = readLine()!!.toInt()
-    val arr = Array(n){ IntArray(n) }
+    fun minimumPushes(word: String): Int {
+        var ans = 0
 
-    for(r in 0 until n){
-        for(c in 0 until n){
-
-            val set = BooleanArray(2 * n )
-
-            //build left set
-            for(i in 0 until c){
-                set[arr[r][i]] = true
-            }
-
-            //build above set
-            for(i in 0 until r){
-                set[arr[i][c]] = true
-            }
-
-            //assign unused mex
-            var mex = 0
-            while(set[mex]) mex++
-            arr[r][c] = mex
+        for (i in word.indices) {
+            ans += (i / 8) + 1
         }
 
-        arr[r].forEach{ print("$it ")}
+        return ans
     }
+
 }
 
 
