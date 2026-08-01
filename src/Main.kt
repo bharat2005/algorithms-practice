2c2f85202ec0372f662c6af84ba7770c8488a291
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-02 01:08:58 +0530

    mex 1.2

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,11 @@
 
 fun main() {
-    fun predictTheWinner(nums: IntArray): Boolean {
-        var p1 = 0
-        var p2 = 0
-        var start = 0
-        var end = nums.size - 1
-        var p1turn = true
-
-        while(start <= end){
-            if(nums[start] >= nums[end]){
-                if(p1turn) p1+= nums[start++] else p2 += nums[start++]
-            } else {
-                if(p1turn) p1+= nums[end--] else p2 += nums[end--]
-            }
-            p1turn = !p1turn
+    val n = readLine()!!.toInt()
+    for(i in 0 until n){
+        Array(n){j ->
+            print("${i or j} ")
         }
-
-
-
-        return p1 >= p2
-
+        println()
     }
 }
 
