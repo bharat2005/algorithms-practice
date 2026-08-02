a38a7a42ae57acd033cc942e68efa0bf22537275
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-02 21:26:19 +0530

    color g 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,36 +1,23 @@
+fun main(){
+    val (m, n) = readLine()!!.split(" ").map { it.toInt() }
+    val grid = Array(m) {
+        readLine()!!.toCharArray()
+    }
+    val charArr = charArrayOf('A', 'B', 'C', 'D')
 
-fun main() {
-    val n = readLine()!!.toInt()
-    val arr = Array(n){ IntArray(n){ -1 } }
-    arr[0][0] = 0
-    val que = ArrayDeque<Pair<Int, Int>>()
-    que.add(Pair(0,0))
+    for(i in 0 until m){
+        for(j in 0 until n){
+            val curr = grid[i][j]
+            val top = if(i - 1 in 0..<m) grid[i - 1][j] else curr
+            val left = if(j-1 in 0..<n) grid[i][j-1] else curr
 
-    val dr = listOf<Int>(2,2,-2,-2,1,-1,-1,1)
-    val dc = listOf<Int>(1,-1,-1,1,2,2,-2,-2)
-    while(que.isNotEmpty()) {
-        val (r, c) = que.removeFirst()
+            var x = 0
+            while(charArr[x] != curr && charArr[x] != top && charArr[x] != left) x++
 
-        //explore all directions
-        for(i in 0 until 8){
-            val nr = r + dr[i]
-            val nc = c + dc[i]
-            if(nr !in 0..<n || nc !in 0..<n) continue
-
-            if(arr[nr][nc] == -1){
-                arr[nr][nc] = arr[r][c] + 1
-                que.add(Pair(nr,nc))
-            }
+            grid[i][j] = charArr[x]
         }
+        println(grid[i].joinToString(" "))
     }
-
-    arr.forEach{
-        it.forEach { print("$it ")}
-        println()
-    }
-
-
 }
-
 
 
