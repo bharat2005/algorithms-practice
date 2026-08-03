b4035531039ac814367a50fef9ee8920032a3d78
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-03 15:41:04 +0530

    feat(grid): implement character transformation logic in Java and Kotlin

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        char[][] grid = new char[m][];
+        for (int i = 0; i < m; i++) {
+            grid[i] = br.readLine().toCharArray();
+        }
+
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char current = grid[i][j];
+                if (i + j % 2 == 0) {
+                    grid[i][j] = (current == 'A') ? 'B' : 'A';
+                } else {
+                    grid[i][j] = (current == 'C') ? 'D' : 'C';
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (j > 0) out.append(' ');
+                out.append(grid[i][j]);
+            }
+            out.append('\n');
+        }
+
+        System.out.print(out);
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,19 @@
 fun main(){
+    val (m, n) = readLine()!!.split(" ").map { it.toInt() }
+    val grid = Array(m) { readLine()!!.toCharArray() }
 
+    for(i in 0 until m){
+        for(j in 0 until n){
+            val current = grid[i][j]
+            if( i + j % 2 == 0){
+                grid[i][j] = if(current == 'A') 'B' else 'A'
+            } else {
+                grid[i][j] = if(current == 'C') 'D' else 'C'
+            }
 
+        }
+        println(grid[i].joinToString(" "))
+    }
 }
 
 
