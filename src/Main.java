f658c72f13f04947346895e95ab1513ac9073afa
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-04 15:09:25 +0530

    inf str idx 2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,40 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FastReader fs = new FastReader();
+        StringBuilder out = new StringBuilder();
 
-        int q = Integer.parseInt(br.readLine());
+        int q = fs.nextInt();
 
         while (q-- > 0) {
-            long k = Long.parseLong(br.readLine());
-
+            long k = fs.nextLong();
             long count = 9L;
             long digits = 1L;
 
             while (k > count * digits) {
+                k -= count * digits;
                 count *= 10;
                 digits++;
             }
@@ -26,10 +47,12 @@ public class Main {
                 idxDig = s.charAt(s.length() - 1);
             } else {
                 String s = Long.toString(num + 1);
-                idxDig = s.charAt((int) (k % digits));
+                idxDig = s.charAt((int) (k % digits) - 1);
             }
 
-            System.out.println(idxDig);
+            out.append(idxDig).append('\n');
         }
+
+        System.out.print(out);
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,17 +2,18 @@ fun main() {
     val q = readLine()!!.toInt()
 
     repeat(q){
-        val k = readLine()!!.toLong()
+        var k = readLine()!!.toLong()
         var count = 9L
         var digits = 1L
 
         while(k > count * digits){
+            k -=  count * digits
             count *= 10
             digits++
         }
 
         val num = count / 9 - 1 + k / digits
-        val idxDig = if(k % digits == 0L) num.toString()[num.toString().length - 1] else (num+1).toString()[(k % digits).toInt()]
+        val idxDig = if(k % digits == 0L) num.toString()[num.toString().length - 1] else (num+1).toString()[(k % digits).toInt() - 1]
 
         println(idxDig)
 
