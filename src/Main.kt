cabc548c8f6d7a2474edc5a30dbc10b908deba53
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-15 18:01:10 +0530

    feat(Main.kt): process input strings case-insensitively
    Converts all input strings to lowercase before adding them to the map.
    This ensures that variations in casing, such as "Apple" and "apple",
    are treated as the same entry, providing accurate counts.

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,13 @@
+import java.util.Locale
+import java.util.Locale.getDefault
+
 fun main(){
     val n = readLine()!!.toInt()
     val map = HashMap<String,Int>()
     var max = Int.MIN_VALUE
 
     repeat(n){
-        val str = readLine()!!
+        val str = readLine()!!.lowercase()
         val nVal =map.getOrDefault(str,0) + 1
         map[str] = nVal
         max = maxOf(max,nVal)
