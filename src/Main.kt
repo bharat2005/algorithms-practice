4799baacab73cba599c763de96eb79204911abf3
bharat2005 <bharatvdeshm2005@gmail.com>
2026-08-19 23:48:06 +0530

    feat(Main.kt): solve min operations to form arithmetic progression
    Replaces previous product calculation and conditional output logic.
    Implements a Depth-First Search (DFS) algorithm.
    Determines the minimum number of operations to make an array an arithmetic progression.
    Explores all possible initial values and common differences.
    Accounts for modifications to array elements to achieve the progression.
    Handles edge cases for array length and unsolvable scenarios.

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,60 @@
+import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyles
 
 fun main(){
-    val t = readLine()!!.toInt()
+    val n = readLine()!!.toInt()
+    val arr = readLine()!!.split(" ").map { it.toInt() }
+    var min = Int.MAX_VALUE
 
-    repeat(t){
-        val n = readLine()!!.toInt()
-        var product= 1
-        val arr = readLine()!!.split(" ").map {
-            if(it.toInt() != 1 || it.toInt() != 0) product *= it.toInt()
-            it.toInt()
-        }.sorted()
-
-        if(arr[0] == 0) {
-            println(product)
+    fun dfs(index : Int, prev : Int, ops : Int, diff : Int){
+        if(index == n){
+            min = minOf(min, ops)
             return
         }
-        if(arr[0] == 1){
-            println(product * 2)
-            return
+        val curr = arr[index]
+        val d1 = curr - prev
+        val d2 = curr + 1 - prev
+        val d3 = curr - 1 - prev
+
+        if(diff == d1 ){
+            dfs(index + 1, curr, ops, diff)
+        } else if(diff == d2){
+            dfs(index + 1, curr + 1, ops+1, diff)
+        } else if(diff == d3){
+            dfs(index + 1, curr - 1, ops+1, diff)
         }
-
-        println((product / arr[n - 1]) * (arr[n-1] + 1))
-
     }
+
+
+    if(arr.size == 1){
+        println(-1)
+        return
+    }
+
+
+    val curr1 = arr[0]
+    val curr2 = curr1 + 1
+    val curr3 = curr1 - 1
+
+    val next1 = arr[1]
+    val next2 = next1 + 1
+    val next3 = next1 - 1
+
+    //curr1
+    dfs(1,curr1, 0, next1 - curr1 )
+    dfs(1,curr1, 0, next2 - curr1 )
+    dfs(1,curr1, 0, next3 - curr1 )
+
+    //curr2
+    dfs(1,curr2, 1, next1 - curr2 )
+    dfs(1,curr2, 1, next2 - curr2 )
+    dfs(1,curr2, 1, next3 - curr2 )
+
+    //curr3
+    dfs(1,curr3, 1, next1 - curr3 )
+    dfs(1,curr3, 1, next2 - curr3 )
+    dfs(1,curr3, 1, next3 - curr3 )
+
+
+    println(if(min == Int.MAX_VALUE) -1 else min)
 
 }
