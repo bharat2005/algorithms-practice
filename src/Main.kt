568d78690ef9297489f44a6aeb172b66562ff49f
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-02 11:46:19 +0530

    refactor(shortestBeautifulSubstring): optimize algorithm for finding substring
    Replaces prefix sum array with direct tracking of '1' indices.
    Iterates through windows of `k` '1's instead of all substrings.
    Improves time complexity by reducing redundant substring checks.
    Simplifies logic for finding the shortest lexicographically smallest substring.

--- src/Main.kt
+++ src/Main.kt
@@ -4,54 +4,35 @@ fun main() {
     fun shortestBeautifulSubstring(s: String, k: Int): String {
         val n = s.length
 
-        //build prefix sum arr
-        val sfx = IntArray(n)
-        sfx[0] = if (s[0] == '1') 1 else 0
-        for (i in 1 until n) {
-            val curr = if (s[i] == '1') 1 else 0
-            sfx[i] = curr + sfx[i - 1]
+        //build ones indices array
+        val ones = mutableListOf<Int>()
+        for(i in s.indices){
+            if(s[i] == '1') ones.add(i)
         }
+        if(ones.size < k) return ""
 
 
-        //try every substring
-        var minLen = Int.MAX_VALUE
-        var minSubstr = s
-        for (i in 0 until n) {
-            for (j in 0 until n) {
-                val right = sfx[j]
-                val left = if (i != 0) sfx[i - 1] else 0
-                val diff = right - left
+        //try all k len ones substr
+        var minSubStr = s
+        for(i in 0..(ones.size - k)){
+            val start = ones[i]
+            val end = ones[i + k - 1]
 
-                if (diff == k) {
-                    val subStr = s.substring(i, j + 1)
-                    if (subStr.length < minLen) {
-                        minLen = subStr.length
-                        minSubstr = subStr
-                    } else if (subStr.length == minLen && subStr < minSubstr) {
-                        minLen = subStr.length
-                        minSubstr = subStr
-                    }
-                } else if (diff > k) {
-                    break
-                }
+            val subStr = s.substring(start, end+1)
+            val currLen = subStr.length
+            val minLen = minSubStr.length
+            if(currLen < minLen){
+                minSubStr = subStr
+            } else if(currLen == minLen && subStr < minSubStr){
+                minSubStr = subStr
             }
         }
 
 
-        //got better substring than s
-        if (s != minSubstr) {
-            return minSubstr
-        }
+        return minSubStr
 
-        //s might be the only possible substr
-        val s1count = s.count { it == '1' }
-        if(s1count == k){
-            s
-        }
-
-        // no possible substr
-        return ""
     }
+
 }
 
 
