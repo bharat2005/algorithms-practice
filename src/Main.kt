000e51922d1a583c22482c1b533d39da41e5f3f4
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-05 11:31:29 +0530

    refactor(Main.kt): optimize firstStableIndex algorithm
    Restructures the firstStableIndex function for improved efficiency.
    Replaces the separate maxArr calculation with a runningMax variable.
    Calculates minArr from right to left to store the minimum value from
    the current index to the end. Computes the score using runningMax
    (max from start to current) and minArr[i] (min from current to end)
    in a single pass. Returns -1 if no stable index is found, providing
    a more accurate result for this edge case.

--- src/Main.kt
+++ src/Main.kt
@@ -3,43 +3,27 @@ fun main() {
     fun firstStableIndex(nums: IntArray, k: Int): Int {
         val n = nums.size
 
-        //build min arr
         val minArr = IntArray(n)
-        minArr[0] = nums[0]
-        for(i in 1 until n){
+        minArr[n - 1] = nums[n - 1]
+        for (i in n - 2 downTo 0) {
             val curr = nums[i]
-            if(curr < minArr[i-1]){
-                //new min
+            if (curr < minArr[i + 1]) {
                 minArr[i] = curr
             } else {
-                //old min
-                minArr[i] = minArr[i-1]
+                minArr[i] = minArr[i + 1]
             }
         }
 
-        //build max arr
-        val maxArr = IntArray(n)
-        maxArr[n-1] = nums[n-1]
-        for(i in n-2 downTo 0){
-            val curr = nums[i]
-            if(curr > maxArr[i+1]){
-                //new max
-                maxArr[i] = curr
-            } else {
-                //old max
-                maxArr[i] = maxArr[i+1]
-            }
+        var runningMax = Int.MIN_VALUE
+        for (i in 0 until n) {
+            runningMax = maxOf(runningMax, nums[i])
+            val score = runningMax - minArr[i]
+            if(score <= k) return i
         }
 
 
-        for(i in 0 until n){
-            val score = maxArr[i] - minArr[i]
-            if(score <= k){
-                return i
-            }
-        }
 
-        return 0
+        return -1
 
     }
 }
