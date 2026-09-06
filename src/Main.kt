12a80268489258ec42e6f1611bcd70258d9e3712
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-06 12:10:14 +0530

    feat(Main.kt): implement new array processing logic with dynamic boundary check

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,17 @@
 
 fun main() {
-    fun firstStableIndex(nums: IntArray, k: Int): Int {
-        val n = nums.size
+    val n = readLine()!!.toInt()
+    val arr = readLine()!!.split(" ").map { it.toInt() }
 
-        val minArr = IntArray(n)
-        minArr[n - 1] = nums[n - 1]
-        for (i in n - 2 downTo 0) {
-            val curr = nums[i]
-            if (curr < minArr[i + 1]) {
-                minArr[i] = curr
-            } else {
-                minArr[i] = minArr[i + 1]
-            }
+    var bnd = 10
+    for(i in 0 until n){
+        val x = arr[i]
+        if(x > bnd){
+            println("No")
+            return
         }
 
-        var runningMax = Int.MIN_VALUE
-        for (i in 0 until n) {
-            runningMax = maxOf(runningMax, nums[i])
-            val score = runningMax - minArr[i]
-            if(score <= k) return i
-        }
-
-
-
-        return -1
-
+        if(i == bnd-1) bnd += 10
     }
+    println("Yes")
 }
