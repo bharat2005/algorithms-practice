e346b6da3fd8ad12e4419631200eafe43a607fbf
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-07 10:49:42 +0530

    refactor(Main.kt): remove solution for "Consecutive Subsequence" problem feat(Main.kt): add initial setup for "Maximum Possible Value" problem chore(problems): remove KSolution.kt as it is no longer needed

--- src/Main.kt
+++ src/Main.kt
@@ -1,47 +1,12 @@
 
 fun main() {
-    val n = readLine()!!.toInt()
-    val arr = readLine()!!.split(" ").map { it.toInt() }
-    val lenMap = mutableMapOf<Int, Int>() //value to count
-    val indexMap = mutableMapOf<Int, Int>() //value to index
-    val lineageMap = mutableMapOf<Int, Int>() //index to index
+    val (n , w) = readLine()!!.split(" ").map{ it.toInt() }
+    val arr = readLine()!!.split(" ").map{ it.toInt() }
 
-    var maxLen = Int.MIN_VALUE
-    var bestEnd = -1
-    for(i in arr.indices){
-        val x = arr[i]
-        indexMap[x] = i
-
-        if(lenMap.contains(x - 1)){
-            //maintain len
-            lenMap[x] = lenMap[x-1]!! + 1
-
-            //maintain idx lineage
-            val prevIdx = indexMap[x-1]!!
-            lineageMap[i] = prevIdx
-        } else {
-            //default start
-            lenMap[x] = 1
-        }
-
-        //track maxLen last idx
-        if(maxLen < lenMap[x]!!){
-            maxLen = lenMap[x]!!
-            bestEnd = i
-        }
-
+    var maxPossible = -1
+    for(a in arr){
+        maxPossible = w - a
     }
 
-    //build ans
-    val ls = mutableListOf<Int>()
-    var curr = bestEnd
-    while(lineageMap.contains(curr)){
-        ls.add(curr)
-        curr = lineageMap[curr]!!
-    }
-    ls.add(curr)
-
-    println(maxLen)
-    println(ls.reversed().joinToString(" "))
-
+    println(maxPossible)
 }
