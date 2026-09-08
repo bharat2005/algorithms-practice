b5b032aa9f45c546c18f78eb39883d4c1e72c02a
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-08 21:16:00 +0530

    style(KSolution.kt): add blank line for improved readability

--- src/main/kotlin/problems/b_monocarp_and_projects/KSolution.kt
+++ src/main/kotlin/problems/b_monocarp_and_projects/KSolution.kt
@@ -9,6 +9,7 @@ fun main() {
     repeat(t){
         var (x, y, k) = readLine()!!.split(" ").map { it.toLong() }
         var res = 0L
+
         for(i in 0 until k){
             res += x % y
             x++
