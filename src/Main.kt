8444ea7939c6fd6a214ea7e065d4500dda92c281
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-08 21:11:55 +0530

    feat(Main.kt): implement algorithm for sum of modulo operations

--- src/Main.kt
+++ src/Main.kt
@@ -4,19 +4,14 @@ fun main() {
     val t = readLine()!!.toInt()
 
     repeat(t){
-        val n = readLine()!!.toInt()
-        val arr = readLine()!!.split(" ").map { it.toInt() }
-        var need = 0
-        if(arr[0] == 1) need++
-        if(arr[n-1] == 1) need++
-
-        var zeros= 0
-        for(x in arr){
-            if(x == 0) zeros++
+        var (x, y, k) = readLine()!!.split(" ").map { it.toInt() }
+        var res = 0
+        repeat(k){
+            res += x % y
+            x++
+            y++
         }
-
-        if(zeros >= 2) println(need) else println(-1)
-
+        println(res)
     }
 
 
