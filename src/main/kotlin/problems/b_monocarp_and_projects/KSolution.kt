8aaa7300aa31304cd9b1ea3498e31b5d0a3bd2f3
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-08 21:14:04 +0530

    feat(problem-solving): add solution for B. Monocarp and Projects
    Adds a new Kotlin solution for the "B. Monocarp and Projects" problem.
    Updates the IDE configuration to include metadata for this problem.
    The solution calculates the sum of remainders using Long types.

--- src/main/kotlin/problems/b_monocarp_and_projects/KSolution.kt
+++ src/main/kotlin/problems/b_monocarp_and_projects/KSolution.kt
@@ -1,11 +1,14 @@
+package problems.b_monocarp_and_projects
+
 import java.util.Scanner
+
 
 fun main() {
     val t = readLine()!!.toInt()
-    
+
     repeat(t){
-        var (x, y, k) = readLine()!!.split(" ").map { it.toInt() }
-        var res = 0
+        var (x, y, k) = readLine()!!.split(" ").map { it.toLong() }
+        var res = 0L
         repeat(k){
             res += x % y
             x++
@@ -13,6 +16,6 @@ fun main() {
         }
         println(res)
     }
-    
-    
+
+
 }
