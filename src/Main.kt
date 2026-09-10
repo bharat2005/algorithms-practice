df8f430ae70bbdc5dad2142aa257000a8575147c
bharat2005 <bharatvdeshm2005@gmail.com>
2026-09-10 12:02:41 +0530

    feat(Main.kt): add solution for average of subtree problem
    Renames the original `main` function to `main1` to make way for a new
    entry point. Introduces a new `main` function that implements a DFS
    solution for the "Average of Subtree" problem. Corrects the base case
    in the `fac` function to handle `num <= 1`, preventing infinite
    recursion for non-positive inputs.

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
 
-
-fun main() {
+//atc - bloacked matrx
+fun main1() {
     val (h,w,a,b) = readLine()!!.split(" ").map { it.toInt() }
     var res = 0
     val MOD = 1_000_000_007
 
     fun fac(num : Int) : Int {
-        if(num == 1) return 1
+        if(num <= 1) return 1
         return num * fac(num - 1)
     }
 
@@ -27,6 +27,35 @@ fun main() {
     println(res % MOD)
 
 }
+
+
+//lc - node avg ct
+fun main() {
+    fun averageOfSubtree(root: TreeNode?): Int {
+        var ct = 0
+
+        fun dfs(node: TreeNode?): List<Int> {
+            if (node == null) return listOf(0, 0) // sum and denom
+
+            val curr = node.`val`
+            val (sum1, den1) = dfs(node.left)
+            val (sum2, den2) = dfs(node.right)
+
+            val currSum = (sum1 + sum2 + curr)
+            val currDen = (den1 + den2 + 1)
+            val currAvg = currSum / currDen
+
+            if (currAvg == curr) ct++
+            return listOf(currSum, currDen)
+        }
+
+        val (a, b) = dfs(root)
+
+        return ct
+
+    }
+}
+
 
 
 
